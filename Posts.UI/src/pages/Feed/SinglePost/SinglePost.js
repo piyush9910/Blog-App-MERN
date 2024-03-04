410ba7e819b539a1235ba88797b1d8bfd8551cc2
piyush9910 <suripiyush4@gmail.com>
2024-03-04 20:09:40 +0530

    app updated 2

--- Posts.UI/src/pages/Feed/SinglePost/SinglePost.js
+++ Posts.UI/src/pages/Feed/SinglePost/SinglePost.js
@@ -14,7 +14,7 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('https://blog-app-mern-pi.vercel.app:8080/feed/post/' + postId, {
+    fetch('https://blog-app-mern-pi.vercel.app/feed/post/' + postId, {
       headers: {
         Authorization: 'Bearer ' + this.props.token
       }
@@ -29,7 +29,7 @@ class SinglePost extends Component {
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
-          image: 'https://blog-app-mern-pi.vercel.app:8080/' + resData.post.imageUrl,
+          image: 'https://blog-app-mern-pi.vercel.app/' + resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content
         });
