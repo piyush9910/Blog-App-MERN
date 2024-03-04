203aa4a431a3fab030457e373a2e3bc63e10e701
piyush9910 <suripiyush4@gmail.com>
2024-03-04 19:35:10 +0530

    Configs added 2

--- Posts.Base.Express/app.js
+++ Posts.Base.Express/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
